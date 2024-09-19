0ade96846e7aa51d9f6d88585a6ee1ba42acd341
Gor Grigoryan <gorgrigoryan1997@gmail.com>
2024-09-19 19:40:27 +0400

    rename select options

--- src/app/constants/user.ts
+++ src/app/constants/user.ts
@@ -1,6 +1,6 @@
 export enum Roles {
   'ROLE_DOCTOR' = 'doctor',
-  'ROLE_NURSE' = 'nurse',
+  'ROLE_NURSE' = 'requester',
   'ROLE_PATIENT' = 'patient',
   'ROLE_EXPERT' = 'expert',
   'ROLE_ADMIN' = 'admin',
